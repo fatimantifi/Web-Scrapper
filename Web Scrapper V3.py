1912c30ec25f12a2340fa84e51452aceb974bb4e
Professeur Gibaud <mail@professeurgibaud.ovh>
2019-12-18 17:34:24 +0100

    Ajout de Parties, Utilisation de Request avec un headers

--- Web Scrapper V3.py
+++ Web Scrapper V3.py
@@ -3,6 +3,14 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
+"""
+
+Navigation dans la page Web
+
+"""
+
+
+
 url = "https://www.mangareader.net/tate-no-yuusha-no-nariagari/1/2"
 response = requests.get(url)
 
@@ -13,6 +21,30 @@ Im = soup.findAll('img')[0]
 link = Im['src']
 
 download_url = link
+
+
+
+"""
+
+Arriver à télécharger l'image
+
+"""
+
+
+
+req = Request(download_url, headers={'User-Agent': 'Mozilla/5.0'})
+
+
+web = urlretrieve(req)
+web_byte = urlopen(req).read()
+
+webpage = web_byte.decode('utf-8')
+
+
+
+
+
+
 
 
 class AppURLopener(urllib.request.FancyURLopener):

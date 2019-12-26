1811141f0bab7e4401cece8c9630062f707f4d25
Professeur Gibaud <mail@professeurgibaud.ovh>
2019-12-26 17:58:41 +0100

    prise en compte de la fin du manga

--- Web Scrapper.py
+++ Web Scrapper.py
@@ -22,7 +22,7 @@ trunk = "https://www.mangareader.net"
 url = trunk + "/tate-no-yuusha-no-nariagari/1/2"
 response = requests.get(url)
 
-
+url = "https://www.mangareader.net/tate-no-yuusha-no-nariagari/62"
 
 
 
@@ -35,9 +35,13 @@ Recherche de l'image dans la page web
 def Navigate(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
-    Im = soup.findAll('img')[0]
-    link = Im['src']
-    download_url = link
+    if soup.text.find("is not released") <0:
+        Im = soup.findAll('img')[0]
+        link = Im['src']
+        download_url = link
+    else:
+        print("Fin du manga")
+        download_url = "Fin du Manga"
     return [soup, download_url]
 
 
@@ -48,14 +52,15 @@ Télécharger l'image
 """
 
 def Download(download_url,name):
-    #On fait une requête et on cache le fait que l'on est un robot
-    req = urllib.request.Request(download_url, headers={'User-Agent': 'Mozilla/5.0'})
+    if download_url != "Fin du Manga":
+        #On fait une requête et on cache le fait que l'on est un robot
+        req = urllib.request.Request(download_url, headers={'User-Agent': 'Mozilla/5.0'})
 
-    #on prend le content de la page en bytes
-    web_byte = urllib.request.urlopen(req).read()
+        #on prend le content de la page en bytes
+        web_byte = urllib.request.urlopen(req).read()
 
-    #on écrit le content dans un fichier test et on lui file le bon format
-    open(name + '.jpg','wb').write(web_byte)
+        #on écrit le content dans un fichier test et on lui file le bon format
+        open(name + '.jpg','wb').write(web_byte)
 
 
 
@@ -67,11 +72,13 @@ Recherche du imgholder (qui permet d'aller à la page suivante)
 """
 
 def Next(soup):
-    L = soup.find(id = "imgholder")
-    M = L.findChildren()
-    CompteurParcours = 0
-    NewAdd = ParcoursSoup(M)
-    #NouvelleUrl = trunk + NewAdd
+    if download_url != "Fin du Manga":
+        L = soup.find(id = "imgholder")
+        M = L.findChildren()
+        CompteurParcours = 0
+        NewAdd = ListeToString(ParcoursSoup(M,CompteurParcours))
+        NouvelleUrl = trunk + NewAdd
+    NouvelleUrl = "Fin du Manga"
     return NouvelleUrl
 
 
@@ -83,4 +90,9 @@ def ParcoursSoup(M,CompteurParcours):
         NewAdd = ParcoursSoup(M,CompteurParcours)
     return [NewAdd,CompteurParcours]
 
-
+def ListeToString(L):
+    M = L
+    if type(M) == type([]):
+        M = M[0]
+        M = ListeToString(M)
+    return M
